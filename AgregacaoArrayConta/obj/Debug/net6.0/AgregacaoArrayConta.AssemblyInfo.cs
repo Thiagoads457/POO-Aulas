83bf66ab9eddfe4d6bb019843da3190e3eeef1b3
Thiagoads457 <thiagoads457@gmail.com>
2024-03-31 16:44:35 -0300

    1

--- AgregacaoArrayConta/obj/Debug/net6.0/AgregacaoArrayConta.AssemblyInfo.cs
+++ AgregacaoArrayConta/obj/Debug/net6.0/AgregacaoArrayConta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AgregacaoArrayConta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d2bf4d65b25157fd3e496e1e430a271ddefb7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1a1ce9fc50e09f7d6e86348e65551888e5f501")]
 [assembly: System.Reflection.AssemblyProductAttribute("AgregacaoArrayConta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AgregacaoArrayConta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ArrayFuncionario/obj/Debug/net6.0/ArrayFuncionario.AssemblyInfo.cs
+++ ArrayFuncionario/obj/Debug/net6.0/ArrayFuncionario.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArrayFuncionario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77d2bf4d65b25157fd3e496e1e430a271ddefb7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1a1ce9fc50e09f7d6e86348e65551888e5f501")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArrayFuncionario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArrayFuncionario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
