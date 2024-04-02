69239017c9228a0629447f1b2daca8323796981e
Thiagoads457 <thiagoads457@gmail.com>
2024-04-01 21:40:39 -0300

    123

--- ComposicaoNotaFiscal/Program.cs
+++ ComposicaoNotaFiscal/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//função Main()
+
+using ComposicaoNotaFiscal;
+
+ItemNotaFiscal item1 = new ItemNotaFiscal(10);
+ItemNotaFiscal item2 = new ItemNotaFiscal(5);
+
+NotaFiscal nf = new NotaFiscal(1,"01.04.2024");
+nf.AdicionarItens(item1);
+nf.AdicionarItens(item2);
+
+foreach (ItemNotaFiscal itens in nf.VetItens)
+    Console.WriteLine("Quantidade: " + itens.Quantidade);
+
+
