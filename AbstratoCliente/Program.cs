72e6fc8c75dcffb6c0ada3f916a4a149f4198928
Thiagoads457 <thiagoads457@gmail.com>
2024-05-13 19:41:49 -0300

    salvar

---
+++ AbstratoCliente/Program.cs
@@ -0,0 +1,7 @@
+﻿using AbstratoCliente;
+
+//Cliente c = new Cliente();//não instanciará
+
+ClienteFisico cf = new ClienteFisico(1,"Thiago","Rua Dois",1111);
+
+ClienteJuridico cj = new ClienteJuridico(100,"Empresa","Rua Quatro",10000);
