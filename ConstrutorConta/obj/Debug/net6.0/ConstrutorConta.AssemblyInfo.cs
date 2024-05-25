e58a7c612fc6618562edc7b4eeb9631f567b1468
Thiagoads457 <thiagoads457@gmail.com>
2024-05-25 20:37:41 -0300

    25052

--- ConstrutorConta/obj/Debug/net6.0/ConstrutorConta.AssemblyInfo.cs
+++ ConstrutorConta/obj/Debug/net6.0/ConstrutorConta.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConstrutorConta")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5395bab3d09b8d24b4c305950cb0abdc2c3e6e74")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConstrutorConta")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConstrutorConta")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
